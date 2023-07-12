a3f8c0605dc96d18d0d4292a2678041cb691a5ca
sakriymohamed <sakriymohamed@gmail.com>
2023-07-12 16:40:40 -0700

    initializing files

---
+++ tvmaze.js
@@ -0,0 +1,102 @@
+"use strict";
+
+const $showsList = $("#showsList");
+const $episodesArea = $("#episodesArea");
+const $searchForm = $("#searchForm");
+const MOVIE_URL = "http://api.tvmaze.com/search/shows";
+const MISSING_IMAGE = "https://tinyurl.com/tv-missing";
+
+
+/** Given a search term, search for tv shows that match that query.
+ *
+ *  Returns (promise) array of show objects: [show, show, ...].
+ *    Each show object should contain exactly: {id, name, summary, image}
+ *    (if no image URL given by API, put in a default image URL)
+ */
+//TODO: naming from movie to tv
+async function getShowsByTerm(term) {
+  // ADD: Remove placeholder & make request to TVMaze search shows API
+  // grab the movie from api
+  //TODO: for future reference - use Axios and not fetch to get the data
+  // CHANGE 'api key' to movie sits api key
+  // grab movieData
+  const config = { params: { q: term } };
+  const response = await axios.get(MOVIE_URL, config);
+  console.log('Response: ', response);
+  //TODO: for future reference - response data is returned object
+  // (only one .data in this case)
+  // naming plural - shows
+  let shows = response.data.map((shows) => {
+    return {
+      id: shows.show.id,
+      name: shows.show.name,
+      summary: shows.show.summary,
+      image: shows.show.image ? shows.show.image.original : MISSING_IMAGE
+    };
+  });
+  console.log('Movie Result: ', shows);
+  return shows;
+};
+
+
+/** Given list of shows, create markup for each and append to DOM.
+ *
+ * A show is {id, name, summary, image}
+ * */
+//TODO: update alt text for src image
+function displayShows(shows) {
+  $showsList.empty();
+  // src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+  for (const show of shows) {
+    const $show = $(`
+        <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+         <div class="media">
+           <img
+              src = ${show.image}
+              alt="show image"
+              class="w-25 me-3">
+           <div class="media-body">
+             <h5 class="text-primary">${show.name}</h5>
+             <div><small>${show.summary}</small></div>
+             <button class="btn btn-outline-light btn-sm Show-getEpisodes">
+               Episodes
+             </button>
+           </div>
+         </div>
+       </div>
+      `);
+
+    $showsList.append($show);
+  }
+}
+
+
+/** Handle search form submission: get shows from API and display.
+ *    Hide episodes area (that only gets shown if they ask for episodes)
+ */
+
+async function searchShowsAndDisplay() {
+  const term = $("#searchForm-term").val();
+  const shows = await getShowsByTerm(term);
+
+  $episodesArea.hide();
+  displayShows(shows);
+}
+
+$searchForm.on("submit", async function handleSearchForm(evt) {
+  evt.preventDefault();
+  await searchShowsAndDisplay();
+});
+
+
+/** Given a show ID, get from API and return (promise) array of episodes:
+ *      { id, name, season, number }
+ */
+
+// async function getEpisodesOfShow(id) { }
+
+/** Write a clear docstring for this function... */
+
+// function displayEpisodes(episodes) { }
+
+// add other functions that will be useful / match our structure & design
